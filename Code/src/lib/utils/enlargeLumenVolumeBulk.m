c27afe7f06d070fa5502dddb846d9860a10eb2ec
jansanrom <jesusangel.asr@gmail.com>
2022-09-27 16:10:56 +0200

    Update enlargeLumenVolumeBulk.m

--- Code/src/lib/utils/enlargeLumenVolumeBulk.m
+++ Code/src/lib/utils/enlargeLumenVolumeBulk.m
@@ -1,1 +1,19 @@
+% image path (.tif)
+imagesPath = 'F:\jesus\labels\';
 
+imgDirectory = dir(strcat(imagesPath, '*', '.tif'));
+
+% save path
+savePath = 'F:\jesus\savePath\';
+
+for idx=1:length(imgDirectory)
+    
+    fileName = imgDirectory(idx).name;
+
+    labelledImage = readStackTif(strcat(imagesPath, fileName));
+    labelledImage =  enlargeLumenVolume(labelledImage);
+    
+    writeStackTif(labelledImage, strcat(savePath, fileName));
+
+    
+end
