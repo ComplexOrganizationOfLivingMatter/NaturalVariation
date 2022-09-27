956e29e56804bd4f1acb74f839e44079449e6110
jansanrom <jesusangel.asr@gmail.com>
2022-09-27 16:00:46 +0200

    Update mainCellFeaturesExtraction_20xCysts.m

--- Code/mainCellFeaturesExtraction_20xCysts.m
+++ Code/mainCellFeaturesExtraction_20xCysts.m
@@ -41,11 +41,9 @@ else
 end
 
 %% Write table path
-<<<<<<< HEAD
-tablePath = 'F:\jesus\';
-=======
+
 tablePath = 'F:\Carmen\';
->>>>>>> a6700bcfc9943f66dbbfe5953d47185798649889
+
 tablePath = uigetdir(tablePath, 'Select savePath (.xls) path');
 
 %% Select name or automatic (date)
@@ -85,16 +83,13 @@ for cyst=1:length(fixedCystsDir)
     end
     
     %% Read rgStack and imgInfo
-<<<<<<< HEAD
-    [rgStackImg, imgInfo] = readStackTif(strcat(originalImagesPath, cystName, '.tif'));
-    
-=======
+
     try
         [rgStackImg, imgInfo] = readStackTif(strcat(originalImagesPath, cystName, '.tif'));
     catch
         continue
     end
->>>>>>> a6700bcfc9943f66dbbfe5953d47185798649889
+
     %% Extract pixel-micron relation
     xResolution = imgInfo(1).XResolution;
     yResolution = imgInfo(1).YResolution;
@@ -148,15 +143,9 @@ for cyst=1:length(fixedCystsDir)
     
     
     %% ID_Cysts
-<<<<<<< HEAD
-    [cells3dFeatures, ~,~,~] = convertPixelsToMicrons(cells3dFeatures, cells3dFeatures, cells3dFeatures, cells3dFeatures,cells3dFeatures, pixelScale);
+
     aux_table = [table(string(repmat(cystName,size(cells3dFeatures, 1),1))), cells3dFeatures];
-    
-    
-=======
-    
-    aux_table = [table(string(repmat(cystName,size(cells3dFeatures, 1),1))), cells3dFeatures];
->>>>>>> a6700bcfc9943f66dbbfe5953d47185798649889
+
     
     %% build and write table    
     dataTable = [dataTable; aux_table];
@@ -164,5 +153,4 @@ for cyst=1:length(fixedCystsDir)
 end
 
 writetable(dataTable,tablePath);
-
 
