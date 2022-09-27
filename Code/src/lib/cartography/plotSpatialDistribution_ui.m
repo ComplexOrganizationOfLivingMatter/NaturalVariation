f3edff5974ba231d495cdc90a9932132ce50a667
jansanrom <jesusangel.asr@gmail.com>
2022-09-27 16:07:10 +0200

    Update plotSpatialDistribution_ui.m

--- Code/src/lib/cartography/plotSpatialDistribution_ui.m
+++ Code/src/lib/cartography/plotSpatialDistribution_ui.m
@@ -1,17 +1,28 @@
 function plotSpatialDistribution_ui
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % plotSpatialDistribution_ui
-    % Plotting "estampitas" of a single variable - user interface
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % 
+    % Main code for plotting  spatial distribution (stamps)
+    %
+    % Fully automated just select:
+    %
+    % 1.- rgStack (.tif) path: raw confocal images
+    % 2.- labels (.mat or .tif) path: labelled images
+    % 3.- savePath: Path to save the results
+    % 4.- variable to plot
+    % 5.- file save name
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     addpath(genpath('D:\Github\Processing3DSegmentation\'));
     addpath(genpath('D:\Github\NaturalVariation\'));
 
-    rgStackPath = uigetdir('', 'Select rgStack (.tif) path');
-    labelsPath = uigetdir('', 'Select labels (.mat) path');
-    savePath = uigetdir('', 'Select save path');
+    rgStackPath = uigetdir('F:\Carmen\', 'Select rgStack (.tif) path');
+    labelsPath = uigetdir('F:\Carmen\', 'Select labels (.mat or .tif) path');
+    savePath = uigetdir('F:\Carmen\methodology_naturalVariation\allLabelsAndRgCyst95\testEnlargeLumen\3selectedCysts/', 'Select save path');
 
     %% select variable to plot
-    params = ["ID_Cell", "Volume", "EquivDiameter", "PrincipalAxisLength", "ConvexVolume", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "apical_NumNeighs", "apical_Area", "basal_NumNeighs", "basal_Area", "cell_height", "lateral_NumNeighs", "lateral_Area", "average_cell_wall_Area", "std_cell_wall_Area", "scutoids", "apicoBasalTransitions", "surfaceRatio", "betCentrality", "coefCluster"];
+    params = ["ID_Cell", "Volume", "EquivDiameter", "PrincipalAxisLength", "ConvexVolume", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "apical_NumNeighs", "apical_Area", "basal_NumNeighs", "basal_Area", "cell_height", "lateral_NumNeighs", "lateral_Area", "average_cell_wall_Area", "std_cell_wall_Area", "scutoids", "apicoBasalTransitions", "surfaceRatio", "betCentrality", "coefCluster", "GRAY"];
 
     for idx = 1:length(params) 
        param = params{idx};
