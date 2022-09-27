3bc7605ae05506ccb02bd7b164afaad99caa7372
jansanrom <jesusangel.asr@gmail.com>
2022-09-27 16:06:33 +0200

    Update plotSpatialDistributionBULK_ui.m

--- Code/src/lib/cartography/plotSpatialDistributionBULK_ui.m
+++ Code/src/lib/cartography/plotSpatialDistributionBULK_ui.m
@@ -1,14 +1,23 @@
 function plotSpatialDistributionBULK_ui
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % plotSpatialDistribution_ui
-    % Plotting "estampitas" of all variables - user interface
-
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % plotSpatialDistributionBULK_ui
+    % 
+    % Main code for plotting "stamps" of all variables
+    %
+    % Fully automated, inputs:
+    %
+    % 1.- rgStackPath: raw confocal images
+    % 2.- labelsPath: labelled (.mat or .tif) files
+    % 3.- savePath: Path to save results
+    % 4.- saveName: Desired name of output files
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+    
     addpath(genpath('D:\Github\NaturalVariation\'));
     addpath(genpath('D:\Github\Processing3DSegmentation\'));
 
-    rgStackPath = uigetdir('F:\Carmen\plotViolinScartter\4d\', 'Select rgStack (.tif) path');
-    labelsPath = uigetdir('F:\Carmen\plotViolinScartter\4d\', 'Select labels (.mat) path');
-    savePath = uigetdir('F:\Carmen\plotViolinScartter\4d\', 'Select save path');
+    rgStackPath = uigetdir('F:\Carmen\', 'Select rgStack (.tif) path');
+    labelsPath = uigetdir('F:\Carmen\', 'Select labels (.mat or .tif) path');
+    savePath = uigetdir('F:\Carmen\', 'Select save path');
 
     %% select variable to plot
     variables = ["Volume", "EquivDiameter", "ConvexVolume", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "apical_NumNeighs", "apical_Area", "basal_NumNeighs", "basal_Area", "cell_height", "lateral_NumNeighs", "lateral_Area", "average_cell_wall_Area", "std_cell_wall_Area", "scutoids", "apicoBasalTransitions"];
