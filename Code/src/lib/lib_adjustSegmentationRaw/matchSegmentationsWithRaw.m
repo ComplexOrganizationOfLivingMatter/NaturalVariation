dfbe19150f354b9b1fe961265b586610243f535a
jansanrom <jesusangel.asr@gmail.com>
2022-10-13 16:58:57 +0200

    Delete matchSegmentationsWithRaw.m

--- Code/src/lib/lib_adjustSegmentationRaw/matchSegmentationsWithRaw.m
+++
@@ -1,7 +0,0 @@
-%% Match segmented cyst and raw image
-pathRoot = 'E:\Pedro\Cysts\3D segmentation\';
-
-pathRawImages = dir([pathRoot '/*/*/*.tif']);
-pathSegmentedImages = dir([pathRoot '/*/*/Results/3d_layers_info.mat']);
-
-%% Resized cyst
