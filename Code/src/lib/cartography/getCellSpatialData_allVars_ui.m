ca194c8ee3f6b29e2553b598c14a4bc05018a078
jansanrom <jesusangel.asr@gmail.com>
2023-10-11 13:09:12 +0200

    Update getCellSpatialData_allVars_ui.m

--- Code/src/lib/cartography/getCellSpatialData_allVars_ui.m
+++ Code/src/lib/cartography/getCellSpatialData_allVars_ui.m
@@ -14,26 +14,46 @@ function getCellSpatialData_allVars_ui()
     savePath = strcat(savePath, '\');
 
     %% select variable to plot
-    params = ["Volume", "EquivDiameter", "PrincipalAxisLength", "ConvexVolume", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "apical_NumNeighs", "apical_Area", "basal_NumNeighs", "basal_Area", "cell_height", "lateral_NumNeighs", "lateral_Area", "average_cell_wall_Area", "std_cell_wall_Area", "scutoids", "apicoBasalTransitions", "surfaceRatio", "coefCluster", "betCentrality"];
+    params = ["Volume", "PrincipalAxisLength", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "apical_NumNeighs", "apical_Area", "basal_NumNeighs", "basal_Area", "cell_height", "lateral_NumNeighs", "lateral_Area", "average_cell_wall_Area", "std_cell_wall_Area", "scutoids", "apicoBasalTransitions", "surfaceRatio"];
 
+    for idx = 1:length(params) 
+       param = params{idx};
+       struct.(param) = param;
+    end
+    
+    
+    % Let the user pick some of the fields:
+    C = fieldnames(struct);
+    size_wind = [1 50; 1 50; 1 50; 1 50]; % Windows size
+    idx = listdlg('PromptString','Select variable to plot.',...
+                  'SelectionMode','m',...
+                  'ListString',C, 'ListSize',[550,250]);
+  
+    % Show the values of the fields that the user picked:
+    variables = [];
+    for k = 1:numel(idx)
+        variables = [variables, {struct.(C{idx(k)})}];
+    end
         
     %% Ask for saveName
     prompt = 'Enter a saveName: ';
     saveName = input(prompt, 's');
     
     %create table
-    for variableIx=1:length(params)
-        variable = params(variableIx);
-        getCellSpatialDataBulk(rgStackPath, labelsPath, variable, savePath, saveName)
+    for variableIx=1:length(variables)
+        variable = variables(variableIx);
+        getCellSpatialDataBulk(rgStackPath, labelsPath, variable{1}, savePath, saveName)
     end
     
     %JOIN TABLES
-    for variableIx=1:length(params)
+    for variableIx=1:length(variables)
         if variableIx == 1
-            variable = params(variableIx);
+            variable = variables(variableIx);
+            variable = variable{1};
             allVarsTable = readtable(strcat(savePath, saveName, '_', variable, '_spatialData.xls'));
         else
-            variable = params(variableIx);
+            variable = variables(variableIx);
+            variable = variable{1};
             auxTable = readtable(strcat(savePath, saveName, '_', variable, '_spatialData.xls'));
             auxTable = auxTable(strcmp(auxTable.cystID, allVarsTable.cystID),  {char(variable), char(strcat(variable, '_norm')), char(strcat(variable, '_mean')), char(strcat(variable, '_mean_norm'))});
             allVarsTable(:, size(allVarsTable, 2)+1:size(allVarsTable, 2)+4) = auxTable;
@@ -44,5 +64,6 @@ function getCellSpatialData_allVars_ui()
     writetable(allVarsTable,fileName);
     
     end
-
+    
+        
 end
