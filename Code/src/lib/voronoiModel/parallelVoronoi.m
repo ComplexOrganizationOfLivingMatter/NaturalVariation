dc42ef8d1661d220fa045123482318eeef211d18
jansanrom <jesusangel.asr@gmail.com>
2024-03-11 16:36:45 +0100

    Update parallelVoronoi.m

--- Code/src/lib/voronoiModel/parallelVoronoi.m
+++ Code/src/lib/voronoiModel/parallelVoronoi.m
@@ -1,12 +1,12 @@
 function parallelVoronoi()
 
-savePath = '/media/pedro/6TB/jesus/NaturalVariation/voronoiModel_9/';
-cells = [500, 750, 1000];
+savePath = '/media/pedro/6TB/jesus/NaturalVariation/voronoiModel_13/';
+cells = [500, 1000];
 axis1 = [75, 100];
 lloyd = 5;
 runs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numLayer = 0;
-cellHeight = 40;
+surfaceRatio = [8,2.45;16,2.08;20,1.91;30,1.52; 50,1.42;100,1.40; 500,1.15;1000,1.03];
 
 
 [ca, cb, cc] = ndgrid(cells, axis1, runs);
@@ -18,13 +18,15 @@ parfor index = 1:size(combs,1)
     currentSeeds = currentData(1);
     currentAxis = currentData(2);
     runId = currentData(3);
+    currentSurfaceRatio = surfaceRatio(find(surfaceRatio(:,1)==currentSeeds), 2);
+    
     try
-        voronoiModel(currentAxis, 100, 100, 750, currentSeeds, lloyd, false, savePath, runId, numLayer, cellHeight)
+        voronoiModel(currentAxis, 100, 100, 750, currentSeeds, lloyd, false, savePath, runId, numLayer, currentSurfaceRatio)
     catch
         disp(strcat('error with ', num2str(currentData)));
         disp('retrying ...')
         try
-            voronoiModel(currentAxis, 100, 100, 750, currentSeeds, lloyd, false, savePath, runId, numLayer, cellHeight)
+            voronoiModel(currentAxis, 100, 100, 750, currentSeeds, lloyd, false, savePath, runId, numLayer, currentSurfaceRatio)
             disp('retry successfull')
         catch
                     disp(strcat('error with ', num2str(currentData)));
