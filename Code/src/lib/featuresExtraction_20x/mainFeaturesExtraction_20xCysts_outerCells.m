c1df7a76aa5ba8369cc122391f838ca787c35768
jansanrom <jesusangel.asr@gmail.com>
2024-03-26 17:37:27 +0100

    Update mainFeaturesExtraction_20xCysts_outerCells.m

--- Code/src/lib/featuresExtraction_20x/mainFeaturesExtraction_20xCysts_outerCells.m
+++ Code/src/lib/featuresExtraction_20x/mainFeaturesExtraction_20xCysts_outerCells.m
@@ -112,61 +112,105 @@ for cyst=1:length(fixedCystsDir)
     basalLayerMask = basalLayer>0;
     se = strel('disk', 5);
     basalLayerMask = imdilate(basalLayerMask, se);
-    labelledImage = labelledImage.*basalLayerMask;
+    labelledImage_BASAL = labelledImage.*basalLayerMask;
     
-    [apicalLayer,basalLayer,lateralLayer,lumenImage] = getApicalBasalLateralAndLumenFromCyst(labelledImage,'');
+    %% BASAL INFO
 
-    %% At least the 0.5% of lateral membrane contacting with other cell to be1 considered as neighbor.
-    contactThreshold = 0.5;
-    dilatedVx = 2;
+        [apicalLayer,basalLayer,lateralLayer,lumenImage] = getApicalBasalLateralAndLumenFromCyst(labelledImage_BASAL,'');
 
-    disp('###################################')
-    disp(strcat('dilatedVx: ', num2str(dilatedVx)))
-    disp(strcat('contactThreshold: ', num2str(contactThreshold)))
-    disp('###################################')
-    
-    try
-        %% Obtain 3D descriptors
-        [allGeneralInfo,allTissues,allLumens,allHollowTissue3dFeatures,allNetworkFeatures,totalMeanCellsFeatures,totalStdCellsFeatures]=calculate3DMorphologicalFeatures(labelledImage,apicalLayer,basalLayer,lateralLayer,lumenImage,path2save,cystName,pixelScale,contactThreshold, [], [], dilatedVx);
-    catch
-        warningCyst = [warningCyst; string(cystName)];
-        warning("on")
-        warning("calculate3DMorphologicalFeatures failed!")    
-        warning("off")
-        continue
-    end
-    %% ID_Cysts
-    allGeneralInfo.Properties.VariableNames{1} = 'ID_Cysts';    
-    
-    %% Lumen data
-    allLumens.ID_Cell = [];
-    allLumens.Properties.VariableNames = cellfun(@(x) strcat('lumen_', x), allLumens.Properties.VariableNames, 'UniformOutput', false);    
-    
-    %% Tissue data
-    allTissues.ID_Cell = [];
-    allTissues.Properties.VariableNames = cellfun(@(x) strcat('tissue_', x), allTissues.Properties.VariableNames, 'UniformOutput', false);    
-    
-    %% Tissue data
-    allHollowTissue3dFeatures.ID_Cell = [];
-    allHollowTissue3dFeatures.Properties.VariableNames = cellfun(@(x) strcat('hollowTissue_', x), allHollowTissue3dFeatures.Properties.VariableNames, 'UniformOutput', false);    
-    
-    %% Cyst Shape
-    cystShape = clasifyCyst(allTissues.tissue_PrincipalAxisLength, 0.1);
-    cystShape = table({cystShape}, 'VariableNames', {'cystShape'});
-    
-    %% perCell Volumes 
-    perCell = table({allTissues.tissue_Volume/allGeneralInfo.NCells_valid}, {allLumens.lumen_Volume/allGeneralInfo.NCells_valid}, {allHollowTissue3dFeatures.hollowTissue_Volume/allGeneralInfo.NCells_valid}, 'VariableNames', {'tissueVolume_perCell', 'lumenVolume_perCell', 'hollowTissueVolume_perCell'});
-    
-    %%  percentage Lumen/Space
-    percentageLumenSpace = table({allLumens.lumen_Volume/allTissues.tissue_Volume}, 'VariableNames', {'percentageLumenSpace'});
-    
-    %% negative  Curvature
-    negativeCurvature = table({evaluateCurvNeg(allTissues.tissue_Solidity, 0.9)}, 'VariableNames', {'negativeCurvature'});
+        %% At least the 0.5% of lateral membrane contacting with other cell to be1 considered as neighbor.
+        contactThreshold = 0.5;
+        dilatedVx = 2;
+
+        disp('###################################')
+        disp(strcat('dilatedVx: ', num2str(dilatedVx)))
+        disp(strcat('contactThreshold: ', num2str(contactThreshold)))
+        disp('###################################')
+
+        try
+            %% Obtain 3D descriptors
+            [allGeneralInfo_BASAL,allTissues_BASAL,allLumens_BASAL,allHollowTissue3dFeatures_BASAL,allNetworkFeatures_BASAL,totalMeanCellsFeatures_BASAL,totalStdCellsFeatures]=calculate3DMorphologicalFeatures(labelledImage_BASAL,apicalLayer,basalLayer,lateralLayer,lumenImage,path2save,cystName,pixelScale,contactThreshold, [], [], dilatedVx);
+        catch
+            warningCyst = [warningCyst; string(cystName)];
+            warning("on")
+            warning("calculate3DMorphologicalFeatures failed!")    
+            warning("off")
+            continue
+        end
+        %% ID_Cysts
+        allGeneralInfo_BASAL.Properties.VariableNames{1} = 'ID_Cysts';    
+
+        %% Lumen data
+        allLumens_BASAL.ID_Cell = [];
+        allLumens_BASAL.Properties.VariableNames = cellfun(@(x) strcat('lumen_', x), allLumens_BASAL.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Tissue data
+        allTissues_BASAL.ID_Cell = [];
+        allTissues_BASAL.Properties.VariableNames = cellfun(@(x) strcat('tissue_', x), allTissues_BASAL.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Tissue data
+        allHollowTissue3dFeatures_BASAL.ID_Cell = [];
+        allHollowTissue3dFeatures_BASAL.Properties.VariableNames = cellfun(@(x) strcat('hollowTissue_', x), allHollowTissue3dFeatures_BASAL.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Cyst Shape
+        cystShape_BASAL = clasifyCyst(allTissues_BASAL.tissue_PrincipalAxisLength, 0.1);
+        cystShape_BASAL = table({cystShape_BASAL}, 'VariableNames', {'cystShape'});
+
+        %% perCell Volumes 
+        perCell_BASAL = table({allTissues_BASAL.tissue_Volume/allGeneralInfo_BASAL.NCells_valid}, {allLumens_BASAL.lumen_Volume/allGeneralInfo_BASAL.NCells_valid}, {allHollowTissue3dFeatures_BASAL.hollowTissue_Volume/allGeneralInfo_BASAL.NCells_valid}, 'VariableNames', {'tissueVolume_perCell', 'lumenVolume_perCell', 'hollowTissueVolume_perCell'});
+
+        %%  percentage Lumen/Space
+        percentageLumenSpace_BASAL = table({allLumens_BASAL.lumen_Volume/allTissues_BASAL.tissue_Volume}, 'VariableNames', {'percentageLumenSpace'});
+
+        %% negative  Curvature
+        negativeCurvature_BASAL = table({evaluateCurvNeg(allTissues_BASAL.tissue_Solidity, 0.9)}, 'VariableNames', {'negativeCurvature'});
+        
+    %% GENERAL INFO
+        labelledImage;
+        
+        try
+            %% Obtain 3D descriptors
+            [allGeneralInfo,allTissues,allLumens,allHollowTissue3dFeatures,allNetworkFeatures,totalMeanCellsFeatures,totalStdCellsFeatures]=calculate3DMorphologicalFeatures(labelledImage,apicalLayer,basalLayer,lateralLayer,lumenImage,path2save,cystName,pixelScale,contactThreshold, [], [], dilatedVx);
+        catch
+            warningCyst = [warningCyst; string(cystName)];
+            warning("on")
+            warning("calculate3DMorphologicalFeatures failed!")    
+            warning("off")
+            continue
+        end
+        %% ID_Cysts
+        allGeneralInfo.Properties.VariableNames{1} = 'ID_Cysts';    
+
+        %% Lumen data
+        allLumens.ID_Cell = [];
+        allLumens.Properties.VariableNames = cellfun(@(x) strcat('lumen_', x), allLumens.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Tissue data
+        allTissues.ID_Cell = [];
+        allTissues.Properties.VariableNames = cellfun(@(x) strcat('tissue_', x), allTissues.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Tissue data
+        allHollowTissue3dFeatures.ID_Cell = [];
+        allHollowTissue3dFeatures.Properties.VariableNames = cellfun(@(x) strcat('hollowTissue_', x), allHollowTissue3dFeatures.Properties.VariableNames, 'UniformOutput', false);    
+
+        %% Cyst Shape
+        cystShape = clasifyCyst(allTissues.tissue_PrincipalAxisLength, 0.1);
+        cystShape = table({cystShape}, 'VariableNames', {'cystShape'});
+
+        %% perCell Volumes 
+        perCell = table({allTissues.tissue_Volume/allGeneralInfo.NCells_valid}, {allLumens.lumen_Volume/allGeneralInfo.NCells_valid}, {allHollowTissue3dFeatures.hollowTissue_Volume/allGeneralInfo.NCells_valid}, 'VariableNames', {'tissueVolume_perCell', 'lumenVolume_perCell', 'hollowTissueVolume_perCell'});
+
+        %%  percentage Lumen/Space
+        percentageLumenSpace = table({allLumens.lumen_Volume/allTissues.tissue_Volume}, 'VariableNames', {'percentageLumenSpace'});
+
+        %% negative  Curvature
+        negativeCurvature = table({evaluateCurvNeg(allTissues.tissue_Solidity, 0.9)}, 'VariableNames', {'negativeCurvature'});
+        
     
     %% build and write table
 %     aux_table = [allGeneralInfo, allTissues, allHollowTissue3dFeatures, allLumens, allNetworkFeatures, totalMeanCellsFeatures, totalStdCellsFeatures, cystShape, negativeCurvature, perCell, percentageLumenSpace];
     
-    aux_table = [allGeneralInfo.ID_Cysts, nCells_wholeTissue, allGeneralInfo.NCells_total, allTissues.tissue_Volume, allTissues.tissue_PrincipalAxisLength, allTissues.tissue_SurfaceArea, totalMeanCellsFeatures.mean_cell_basal_NumNeighs, allTissues.tissue_basal_triangles, allTissues.tissue_basal_squares, allTissues.tissue_basal_pentagons, allTissues.tissue_basal_hexagons, allTissues.tissue_basal_heptagons, allTissues.tissue_basal_octogons, allTissues.tissue_basal_nonagons, totalMeanCellsFeatures.mean_cell_basal_Area, totalMeanCellsFeatures.mean_cell_basalPerimeter];
+    aux_table = [allGeneralInfo_BASAL.ID_Cysts, nCells_wholeTissue, allGeneralInfo.NCells_total, allGeneralInfo_BASAL.NCells_total, allTissues.tissue_Volume, allTissues.tissue_Solidity, allTissues_BASAL.tissue_PrincipalAxisLength, allTissues_BASAL.tissue_SurfaceArea, totalMeanCellsFeatures.mean_cell_Volume, totalMeanCellsFeatures.mean_cell_Solidity, totalMeanCellsFeatures_BASAL.mean_cell_basal_NumNeighs, allTissues_BASAL.tissue_basal_triangles, allTissues_BASAL.tissue_basal_squares, allTissues_BASAL.tissue_basal_pentagons, allTissues_BASAL.tissue_basal_hexagons, allTissues_BASAL.tissue_basal_heptagons, allTissues_BASAL.tissue_basal_octogons, allTissues_BASAL.tissue_basal_nonagons, totalMeanCellsFeatures_BASAL.mean_cell_basal_Area, totalMeanCellsFeatures_BASAL.mean_cell_basalPerimeter];
     cell2table(aux_table);
     dataTable = [dataTable; aux_table];
     
@@ -174,7 +218,7 @@ end
 
 % writetable(dataTable,tablePath); 
 
-dataTable.Properties.VariableNames = {'ID_Cysts', 'nCells_wholeTissue','nCells', 'tissue volume', 'principalAxisLenght', 'tissue_SurfaceArea', 'meanNeighs','tissue_basal_triangles', 'tissue_basal_squares', 'tissue_basal_pentagons', 'tissue_basal_hexagons', 'tissue_basal_heptagons', 'tissue_basal_octogons',   'tissue_basal_nonagons', 'mean_cell_basal_Area', 'mean_cell_basalPerimeter'};
+dataTable.Properties.VariableNames = {'ID_Cysts', 'nCells_wholeTissue','nCells_total', 'nCells_basal', 'tissue_volume', 'tissue_solidity', 'principalAxisLenght', 'tissue_SurfaceArea', 'mean_cell_volume', 'mean_cell_solidity', 'meanNeighs','tissue_basal_triangles', 'tissue_basal_squares', 'tissue_basal_pentagons', 'tissue_basal_hexagons', 'tissue_basal_heptagons', 'tissue_basal_octogons',   'tissue_basal_nonagons', 'mean_cell_basal_Area', 'mean_cell_basalPerimeter'};
 % dataTable_sheet_1 = dataTable_1(:, {'ID_Cysts', 'nCells', 'tissue_SurfaceArea', 'tissue_basal_triangles', 'tissue_basal_squares', 'tissue_basal_pentagons', 'tissue_basal_hexagons', 'tissue_basal_heptagons', 'tissue_basal_octogons',   'tissue_basal_nonagons', 'mean_cell_basal_Area', 'mean_cell_basalPerimeter'});
 writetable(dataTable,tablePath,'Sheet','basalInfo');
 % dataTable_sheet_2 = dataTable(:, {'ID_Cysts', 'tissue_apical_triangles', 'tissue_apical_squares', 'tissue_apical_pentagons', 'tissue_apical_hexagons', 'tissue_apical_heptagons', 'tissue_apical_octogons', 'tissue_apical_nonagons', 'tissue_apical_decagons', 'tissue_basal_triangles', 'tissue_basal_squares', 'tissue_basal_pentagons', 'tissue_basal_hexagons', 'tissue_basal_heptagons', 'tissue_basal_octogons', 'tissue_basal_nonagons', 'tissue_basal_decagons', 'tissue_lateral_triangles', 'tissue_lateral_squares', 'tissue_lateral_pentagons', 'tissue_lateral_hexagons', 'tissue_lateral_heptagons', 'tissue_lateral_octogons', 'tissue_lateral_nonagons', 'tissue_lateral_decagons'});
