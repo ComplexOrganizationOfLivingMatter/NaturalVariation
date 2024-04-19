a9fe5f8a16462dbe28a99e361f9e8325e0d5de90
jansanrom <jesusangel.asr@gmail.com>
2024-04-19 11:13:15 +0200

    Add files via upload

---
+++ Code/src/lib/cartography/getCellSpatialDataBulk_multilayer.m
@@ -0,0 +1,215 @@
+function getCellSpatialDataBulk_multilayer(originalImagesPath, fixedCystsPath, variable, savePath, saveName)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % getCellSpatialDataBulk
+    % Function that calls getCellSpatialData and get SpatialData for each cyst
+    % THIS FUNCTION IS INTENDED TO BE LAUNCHED USING THE HOMONIMOUS _UI FILE!
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % inputs:
+    % originalImagesPath: path for raw images
+    % fixedCystsPath: path for labels
+    % variable: Name of the variable e.g. "cell_height"
+    % savePath: path for saving table
+    % saveName: table's name
+    
+    %% Directory
+    fixedCystsDir = dir(strcat(fixedCystsPath, '*.mat'));
+    if isempty(fixedCystsDir)
+        fixedCystsDir = dir(strcat(fixedCystsPath, '*.tif'));
+        formatFlag = '.tif';
+    else
+        formatFlag = '.mat';
+    end
+    
+    % arrays for table
+    variableArray = [];
+    typeArray = [];
+    classArray = [];
+    cystIDArray = [];
+    cystShapeArray = [];
+    zPosArray = [];
+    normVariableDataArray = [];
+    variableDataArray = [];
+    normZPosArray = [];
+    cellIDsArray = [];
+    negativeCurvatureArray = [];
+    nCellsArray = [];
+    normXYPosArray = [];
+    xyPosArray = [];
+    zPosCentroidArray = [];
+    polarDistrArray = [];
+    polarDistArray = [];
+    variableMeanArray = [];
+    normVariableMeanArray = [];
+    
+    for cyst=1:length(fixedCystsDir)
+
+        %% Extract cyst name
+        cystName = fixedCystsDir(cyst).name;
+        if strcmp(formatFlag, '.mat')
+            cystName = strsplit(cystName, '.mat');
+        else
+            cystName = strsplit(cystName, '.tif');
+        end
+
+        cystName = cystName{1};
+        disp(cystName)
+        
+        %% Load labels
+        if strcmp(formatFlag, '.mat')
+            load(strcat(fixedCystsPath, cystName, '.mat'), 'labelledImage');
+        else
+            labelledImage = readStackTif(strcat(fixedCystsPath, cystName, '.tif'));
+        end
+        
+        %% Read rgStack and imgInfo
+        [rgStackImg, imgInfo] = readStackTif(strcat(originalImagesPath, cystName, '.tif'));
+
+        %% Extract pixel-micron relation
+        xResolution = imgInfo(1).XResolution;
+        yResolution = imgInfo(1).YResolution;
+        spacingInfo = strsplit(imgInfo(1).ImageDescription, 'spacing=');
+        spacingInfo = strsplit(spacingInfo{2}, '\n');
+        z_pixel = str2num(spacingInfo{1});
+        x_pixel = 1/xResolution;
+        y_pixel = 1/yResolution;
+        
+        pixelScale = x_pixel;
+
+        %% Get original image size
+        shape = size(rgStackImg);
+
+        %% Make homogeneous
+        numRows = shape(1);
+        numCols = shape(2);
+        numSlices = round(shape(3)*(z_pixel/x_pixel));
+
+        labelledImage = imresize3(labelledImage, [numRows, numCols, numSlices], 'nearest');
+
+        principalAxisLength = regionprops3(labelledImage>1, 'PrincipalAxisLength');
+        cystClassification = clasifyCyst(principalAxisLength.PrincipalAxisLength, 0.1);
+        
+        [apicalLayer,basalLayer,lateralLayer,lumenImage] = getApicalBasalLateralAndLumenFromCyst(labelledImage,'');
+        
+        %% At least the 0.5% of lateral membrane contacting with other cell to be1 considered as neighbor.
+        contactThreshold = 0.5;
+        dilatedVx = 2;
+
+        validCells = find(table2array(regionprops3(labelledImage,'Volume'))>0);
+        noValidCells = [];
+        
+        validCells_size = find(table2array(regionprops3(labelledImage,'Volume'))>0);
+        validCells_apicoBasal = intersect(unique(apicalLayer),unique(basalLayer));
+        validCells = intersect(unique(validCells_size), unique(validCells_apicoBasal));
+        noValidCells = setdiff(unique(labelledImage), validCells);
+
+        try
+            
+            %% BASAL INFO: NEIGHBOURS, PERIMETER, AREA, 
+            [basal3dInfo] = calculateNeighbours3D(basalLayer, dilatedVx, basalLayer == 0);
+            basal3dInfo = basal3dInfo.neighbourhood;
+            basalCells = unique(basalLayer);
+            basalCells = basalCells(basalCells>0);
+            
+            [~, basalPerimeter, ~, basalNeighsOfNeighs, ~] = calculatePerimeters(basalCells, [], [], basalLayer, basal3dInfo, lateralLayer, []);
+            
+            basal_area_cells=cell2mat(struct2cell(regionprops(basalLayer,'Area'))).';
+            
+            %% CONVEX VOLUME, SOLIDITY, ASPECT RATIO, SPHERICITY, NORMALIZEDVOLUME, IRREGULARITYSHAPE INDEX, 
+            cells3dFeatures = extract3dDescriptors(labelledImage, unique(labelledImage));
+            cells3dFeatures = cells3dFeatures(~strcmpi(cells3dFeatures.ID_Cell,'cell_0'), :);
+            
+            cells3dFeatures.basalPerimeter = repmat(0, size(cells3dFeatures,1), 1);
+            cells3dFeatures.basalNeighsOfNeighs = repmat(0, size(cells3dFeatures,1), 1);
+            cells3dFeatures.basal_NumNeighs = repmat(0, size(cells3dFeatures,1), 1);
+            cells3dFeatures.basal_Area = repmat(0, size(cells3dFeatures,1), 1);
+            
+            for cellIx = 1:size(cells3dFeatures,1)
+                currentCellId = cells3dFeatures.ID_Cell(cellIx);
+                currentCellId = currentCellId{1};
+                currentCellId = strsplit(currentCellId, '_');
+                currentCellId = str2num(currentCellId{2});
+                
+                if ismember(currentCellId, basalCells)
+                    cells3dFeatures.basalperimeter(cellIx) = basalPerimeter(find(basalCells==currentCellId));
+                    cells3dFeatures.basal_Area(cellIx) = basal_area_cells(find(basalCells==currentCellId));
+                    cells3dFeatures.basalNeighsOfNeighs(cellIx) = (basalNeighsOfNeighs(find(basalCells==currentCellId)));
+                    cells3dFeatures.basal_NumNeighs(cellIx) = length(basal3dInfo{find(basalCells==currentCellId)});
+                end
+            end
+                        
+            %% JOIN TABLES
+            cells3dFeatures.totalBasalArea = repmat(sum(cells3dFeatures.basal_Area), size(cells3dFeatures, 1), 1);
+            
+            [cells3dFeatures, ~,~,~] = convertPixelsToMicrons(cells3dFeatures, cells3dFeatures, cells3dFeatures, cells3dFeatures,cells3dFeatures, pixelScale);
+            
+        catch
+            continue
+        end
+
+        data = cells3dFeatures(:, variable).Variables;
+        
+        cellIDArray = cells3dFeatures.ID_Cell;
+                
+        try
+            [normZPos, zPos, normVariableData, variableData, xyPos, normXYPos, zPosCentroid, polarDist, polarDistr] = getCellSpatialData(labelledImage, data, cellIDArray, variable, pixelScale);
+        catch
+            warning('check segmentation')
+            continue
+        end
+        
+        [tissue3dFeatures] = extract3dDescriptors(labelledImage>0|lumenImage>0, 1);
+
+        cystShape = clasifyCyst(tissue3dFeatures.PrincipalAxisLength, 0.1);
+        negativeCurvature = {evaluateCurvNeg(tissue3dFeatures.Solidity, 0.9)};
+
+        cystIDArray = [cystIDArray; repmat({cystName}, [size(normZPos,2), 1])];
+        cystShapeArray = [cystShapeArray; repmat({cystShape}, [size(normZPos,2), 1])];
+        negativeCurvatureArray = [negativeCurvatureArray; repmat({negativeCurvature}, [size(normZPos,2), 1])];
+        nCellsArray = [nCellsArray; repmat({length(validCells)}, [size(normZPos,2), 1])];
+        variableMeanArray = [variableMeanArray; repmat({mean(variableData)}, [size(normZPos,2), 1])];
+        normVariableMeanArray = [normVariableMeanArray; repmat({mean(normVariableData)}, [size(normZPos,2), 1])];
+
+        cellIDsArray = [cellIDsArray, cellIDArray'];
+        normZPosArray = [normZPosArray, normZPos];      
+        zPosCentroidArray = [zPosCentroidArray, zPosCentroid];        
+        zPosArray = [zPosArray, zPos];     
+        normXYPosArray = [normXYPosArray, normXYPos];        
+        xyPosArray = [xyPosArray, xyPos];   
+        normVariableDataArray = [normVariableDataArray, normVariableData];  
+        variableDataArray = [variableDataArray, variableData];
+        polarDistArray = [polarDistArray, polarDist];
+        polarDistrArray = [polarDistrArray, polarDistr];
+
+
+
+end
+
+    spatialDataTable.cystID = cellfun(@(x) string(x), cystIDArray);
+    spatialDataTable.cellID = cellIDsArray';
+    spatialDataTable.cystShape = cellfun(@(x) string(x), cystShapeArray);
+    spatialDataTable.cystCurvature = cellfun(@(x) string(x), negativeCurvatureArray);
+    spatialDataTable.nCells = nCellsArray;
+    spatialDataTable.normZPos = normZPosArray';
+    spatialDataTable.zPos = zPosArray';
+    spatialDataTable.zPosCentroid = zPosCentroidArray';
+    spatialDataTable.normXYPos = normXYPosArray';
+    spatialDataTable.xyPos = xyPosArray';
+    spatialDataTable.polarDist = polarDistArray';
+    spatialDataTable.polarDistr = polarDistrArray';
+    spatialDataTable.variableData = variableDataArray';
+    spatialDataTable.variableDataMean = variableMeanArray;
+    spatialDataTable.normVariableData = normVariableDataArray';
+    spatialDataTable.normVariableDataMean = normVariableMeanArray;
+
+
+    spatialDataTable = struct2table(spatialDataTable);
+    spatialDataTable.Properties.VariableNames{'variableData'} = char(variable);
+    spatialDataTable.Properties.VariableNames{'normVariableData'} = char(strcat(variable, '_norm'));
+    spatialDataTable.Properties.VariableNames{'variableDataMean'} = char(strcat(variable, '_mean'));
+    spatialDataTable.Properties.VariableNames{'normVariableDataMean'} = char(strcat(variable, '_mean_norm'));
+
+    fileName = strcat(savePath, saveName, '_', variable, '_spatialData.xls');
+    writetable(spatialDataTable,fileName);
+    
+
+end
---
+++ Code/src/lib/cartography/getCellSpatialData_allVars_multilayer_ui.m
@@ -0,0 +1,74 @@
+function getCellSpatialData_allVars_multilayer_ui()
+
+    addpath(genpath('D:\Github\NaturalVariation\'));
+
+    %ui get path rgStack
+    rgStackPath = uigetdir('F:\jesus', 'Select rgStack (.tif) path');
+    rgStackPath = strcat(rgStackPath, '\');
+%     rgStackPath = '/home/pedro/Escritorio/testEMB/testOrgP_ErrorAllvars/rgStack/';
+    %ui get path labels
+    labelsPath = uigetdir('F:\jesus', 'Select labels (.mat) path');
+    labelsPath = strcat(labelsPath, '\');
+%     labelsPath = '/home/pedro/Escritorio/testEMB/testOrgP_ErrorAllvars/fixed/';
+
+    %ui get path to save
+    savePath = uigetdir('F:\jesus', 'Select save path');
+    savePath = strcat(savePath, '\');
+%     savePath = '/home/pedro/Escritorio/testEMB/testOrgP_ErrorAllvars/';
+
+    %% select variable to plot
+    params = ["Volume", "PrincipalAxisLength", "Solidity", "SurfaceArea", "aspectRatio", "sphericity", "normalizedVolume", "irregularityShapeIndex", "basal_NumNeighs", "basal_Area", "basalPerimeter", "basalNeighsOfNeighs", "totalBasalArea"];
+
+    for idx = 1:length(params) 
+       param = params{idx};
+       struct.(param) = param;
+    end
+    
+    % Let the user pick some of the fields:
+    C = fieldnames(struct);
+    size_wind = [1 50; 1 50; 1 50; 1 50]; % Windows size
+    idx = listdlg('PromptString','Select variable to plot.',...
+                  'SelectionMode','m',...
+                  'ListString',C, 'ListSize',[550,250]);
+  
+    % Show the values of the fields that the user picked:
+    variables = [];
+    for k = 1:numel(idx)
+        variables = [variables, {struct.(C{idx(k)})}];
+    end
+        
+    variables = params;
+
+    %% Ask for saveName
+    prompt = 'Enter a saveName: ';
+    saveName = input(prompt, 's');
+    saveName= 'test';
+    
+    %create table
+    for variableIx=1:length(variables)
+        variable = variables(variableIx);
+        disp(variable)
+        getCellSpatialDataBulk_multilayer(rgStackPath, labelsPath, variable{1}, savePath, saveName);
+    end
+    
+    %JOIN TABLES
+    for variableIx=1:length(variables)
+        if variableIx == 1
+            variable = variables(variableIx);
+            variable = variable{1};
+            allVarsTable = readtable(strcat(savePath, saveName, '_', variable, '_spatialData.xls'));
+        else
+            variable = variables(variableIx);
+            variable = variable{1};
+            auxTable = readtable(strcat(savePath, saveName, '_', variable, '_spatialData.xls'));
+            auxTable = auxTable(strcmp(auxTable.cystID, allVarsTable.cystID),  {char(variable), char(strcat(variable, '_norm')), char(strcat(variable, '_mean')), char(strcat(variable, '_mean_norm'))});
+            allVarsTable(:, size(allVarsTable, 2)+1:size(allVarsTable, 2)+4) = auxTable;
+            allVarsTable.Properties.VariableNames = [allVarsTable.Properties.VariableNames(1:size(allVarsTable,2)-4), auxTable.Properties.VariableNames];
+        end
+    fileName = strcat(savePath, saveName, '_allVars_spatialData.xls');
+    writetable(allVarsTable,fileName);
+    
+    end
+    
+        
+end
