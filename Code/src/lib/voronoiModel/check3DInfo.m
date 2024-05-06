5e4d124a51b2c48906688551ce37165daecbdd0d
jansanrom <jesusangel.asr@gmail.com>
2024-05-06 17:22:01 +0200

    filter scutoids

--- Code/src/lib/voronoiModel/check3DInfo.m
+++ Code/src/lib/voronoiModel/check3DInfo.m
@@ -1,7 +1,7 @@
 function check3DInfo()
 
 % path = '/media/pedro/6TB/jesus/NaturalVariation/voronoi_SELECTED/bySeeds/';
-path = '/media/pedro/6TB/jesus/NaturalVariation/voronoi_SELECTED/bySeeds/30/';
+path = '/media/pedro/6TB/jesus/NaturalVariation/voronoi_SELECTED/bySeeds/1000/';
 
 % matDir = dir(strcat(path, '*/*/*.mat'));
 matDir = dir(strcat(path, '*/*.mat'));
@@ -25,7 +25,7 @@ for dirIx = 1:numel(matDir)
         fullbasalxlsWritePath = strcat(folder, '/', nameClean, 'BASAL', '.xls');
         fullbasalmatWritePath = strcat(folder, '/', nameClean, 'BASAL', '.mat');
 
-        full3DInfoPath = strcat(folder, '/', nameClean, '3D_info', '.xls');
+        full3DInfoPath = strcat(folder, '/', nameClean, '3D_info_filteredScutoids', '.xls');
             
         if exist(full3DInfoPath, 'file')==2 | exist(strrep(full3DInfoPath, 'BASAL', ''), 'file')==2
             disp(strcat('skipping ', nameClean))
@@ -61,8 +61,115 @@ for dirIx = 1:numel(matDir)
                 apicoBasalTransitionsArray = [apicoBasalTransitionsArray; apicoBasalTransitions];
                 scutoidArray = [scutoidArray; scutoid];
            end
+           
+            %% filter scutoids
+           scutoidPosition = find(scutoidArray>0);
+           
+            % 3D neighs (lateral neighs)
+            current3DNeighsArray = [];
+            apicoBasalTransitionsLabels = [];
+            for rowIx = 1:size(tableLayerApical,1)
+                
+                currentApicalNeighs = table2array(tableLayerApical(rowIx, contains(tableLayerApical.Properties.VariableNames, 'neighs_')));
+                currentBasalNeighs = table2array(tableLayerBasal(rowIx, contains(tableLayerBasal.Properties.VariableNames, 'neighs_')));
+                currentApicalNeighs = currentApicalNeighs(~isnan(currentApicalNeighs));
+                currentBasalNeighs = currentBasalNeighs(~isnan(currentBasalNeighs));
+                
+                current3DNeighs = [currentApicalNeighs, currentBasalNeighs];
+                current3DNeighs = current3DNeighs(~isnan(current3DNeighs));
+                current3DNeighs = unique(current3DNeighs);
+                current3DNeighsArray = [current3DNeighsArray; {current3DNeighs'}];
+                
+                currentApicoBasalTransitionsLabels = setxor(currentApicalNeighs, currentBasalNeighs);
+                apicoBasalTransitionsLabels = [apicoBasalTransitionsLabels, {currentApicoBasalTransitionsLabels}];
 
-           dataTable = table(tableLayerApical.id, tableLayerApical.cell_area, tableLayerBasal.cell_area, tableLayerApical.perim_area, tableLayerBasal.perim_area, tableLayerApical.total_ellipsoid_area, tableLayerBasal.total_ellipsoid_area, tableLayerApical.numNeighs, tableLayerBasal.numNeighs, apicoBasalTransitionsArray, scutoidArray, apicalNeighsArray, basalNeighsArray);
+            end
+            
+           newScutoids_cells = zeros(size(scutoidArray));
+           for scutoidIx = 1:numel(scutoidPosition)
+                scutoidId = scutoidPosition(scutoidIx);
+                currentScutoidApicalNeighs = apicalNeighsArray{scutoidId};
+                currentScutoidBasalNeighs = basalNeighsArray{scutoidId};
+                                
+                intersections = arrayfun(@(x) intersect(current3DNeighsArray{x}, current3DNeighsArray{scutoidId}), apicoBasalTransitionsLabels{scutoidId}, 'UniformOutput', false);
+                intersectionsLengths = cellfun(@(x) length(x), intersections)';
+
+                crossedIntersections = cellfun(@(x) arrayfun(@(y) intersect(apicoBasalTransitionsLabels{y}, x), x, 'UniformOutput', false), intersections, 'UniformOutput', false);
+                [crossedIntersectionsNonEmpty] = cellfun(@(x) sum(cell2mat(x)), crossedIntersections)';
+                
+                scutoidCondition = arrayfun(@(x, y) x>=1 && y>=2, crossedIntersectionsNonEmpty, intersectionsLengths);
+
+                if sum(scutoidCondition)>=1
+                    newScutoids_cells(scutoidId) = 1;
+                end
+                
+           end
+            
+           %% replace fixed scutoids but saving the old data
+           oldScutoidArray = scutoidArray;
+           scutoidArray = newScutoids_cells;
+           
+           %% fix neighbours of scutoids that were removed
+            wrongScutoids=arrayfun(@(x, y) setdiff(x,y), oldScutoidArray, scutoidArray, 'UniformOutput', false);
+
+            for scuIx=1:length(wrongScutoids)
+                if wrongScutoids{scuIx}==1
+                   difference = setxor(apicalNeighsArray{scuIx},basalNeighsArray{scuIx});
+                   if size(apicalNeighsArray{scuIx},2) < size(basalNeighsArray{scuIx},2)
+                       
+                       basalNeighsArray{scuIx}=apicalNeighsArray{scuIx};
+                       
+                       for cellIxDiff = 1:length(difference)
+                           cellIdDiff =  difference(cellIxDiff);
+                           oldValues = basalNeighsArray{cellIdDiff};
+                           basalNeighsArray{cellIdDiff} = oldValues(oldValues~=scuIx);
+                       end
+%                      lateral3dInfo_total{scuIx}=apicalNeighsArray{scuIx};
+
+                   elseif size(apicalNeighsArray{scuIx},2) > size(basalNeighsArray{scuIx},2)
+                       apicalNeighsArray{scuIx}=basalNeighsArray{scuIx};
+%                        lateral3dInfo_total{scuIx}=basalNeighsArray{scuIx};
+
+                       for cellIxDiff = 1:length(difference)
+                           cellIdDiff =  difference(cellIxDiff);
+                           oldValues = apicalNeighsArray{cellIdDiff};
+                           apicalNeighsArray{cellIdDiff} = oldValues(oldValues~=scuIx);
+                       end
+                       
+                   else
+                      for cellIxDiff = 1:length(difference)
+                          cellIdDiff = difference(cellIxDiff);
+                          oldValues = apicalNeighsArray{scuIx};
+                          apicalNeighsArray{scuIx} = oldValues(oldValues~=cellIdDiff);
+                          basalNeighsArray{scuIx} = oldValues(oldValues~=cellIdDiff);
+                      end
+                      
+                      for cellIxDiff = 1:length(difference)
+                           cellIdDiff =  difference(cellIxDiff);
+                           oldValues = basalNeighsArray{cellIdDiff};
+                           basalNeighsArray{cellIdDiff} = oldValues(oldValues~=scuIx);
+                      end
+                      
+                      for cellIxDiff = 1:length(difference)
+                           cellIdDiff =  difference(cellIxDiff);
+                           oldValues = apicalNeighsArray{cellIdDiff};
+                           apicalNeighsArray{cellIdDiff} = oldValues(oldValues~=scuIx);
+                      end 
+                      
+                   end
+                   
+                end
+            end           
+           
+            apicalNumNeighs = cellfun(@(x) size(x,2), apicalNeighsArray, 'UniformOutput', false);
+            basalNumNeighs = cellfun(@(x) size(x,2), basalNeighsArray, 'UniformOutput', false);
+            
+            %% apicoBasalTransitions
+            apicoBasalTransitionsArray = cellfun(@(x,y) length(setxor(x, y)), apicalNeighsArray, basalNeighsArray, 'UniformOutput', false);
+            apicoBasalTransitionsArray = cell2mat(apicoBasalTransitionsArray);
+           %%
+
+           dataTable = table(tableLayerApical.id, tableLayerApical.cell_area, tableLayerBasal.cell_area, tableLayerApical.perim_area, tableLayerBasal.perim_area, tableLayerApical.total_ellipsoid_area, tableLayerBasal.total_ellipsoid_area, apicalNumNeighs, basalNumNeighs, apicoBasalTransitionsArray, scutoidArray, apicalNeighsArray, basalNeighsArray);
            dataTable.Properties.VariableNames = {'id', 'apicalCellArea', 'basalCellArea', 'apicalPerim', 'basalPerim', 'apicalTotalArea', 'basalTotalArea', 'apicalNumNeighs', 'basalNumNeighs', 'apicoBasalTransitions', 'scutoid', 'apicalNeighs', 'basalNeighs'};           
 
            writetable(dataTable, full3DInfoPath);
